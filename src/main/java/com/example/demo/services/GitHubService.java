aa892d3645f67f0887697fe366112d0b8de381d3
Przemek <pk.korzen1@gmail.com>
2024-02-21 15:40:13 +0100

    changed service to return DTOs instead of models

--- src/main/java/com/example/demo/services/GitHubService.java
+++ src/main/java/com/example/demo/services/GitHubService.java
@@ -1,11 +1,12 @@
 package com.example.demo.services;
 
-import com.example.demo.model.branch.Branch;
+import com.example.demo.dto.GitHubBranchDTO;
+import com.example.demo.dto.GitHubRepoDTO;
 import com.example.demo.model.repo.GithubRepo;
 
 import java.util.List;
 
 public interface GitHubService {
-    List<GithubRepo> findReposByUsername(String username);
-    List<Branch> findBranchesByUsernameAndRepo(String username, GithubRepo repo);
+    List<GitHubRepoDTO> findReposByUsername(String username);
+    List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, GithubRepo repo);
 }
--- src/main/java/com/example/demo/services/implementations/GitHubServiceImpl.java
+++ src/main/java/com/example/demo/services/implementations/GitHubServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.services.implementations;
 
-import com.example.demo.model.branch.Branch;
+import com.example.demo.dto.GitHubBranchDTO;
+import com.example.demo.dto.GitHubRepoDTO;
 import com.example.demo.model.repo.GithubRepo;
 import com.example.demo.services.GitHubService;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,11 @@ import java.util.List;
 @Service
 public class GitHubServiceImpl implements GitHubService {
     @Override
-    public List<GithubRepo> findReposByUsername(String username) {
+    public List<GitHubRepoDTO> findReposByUsername(String username) {
         return Collections.emptyList();
     }
     @Override
-    public List<Branch> findBranchesByUsernameAndRepo(String username, GithubRepo repo) {
+    public List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, GithubRepo repo) {
         return List.of();
     }
 }
