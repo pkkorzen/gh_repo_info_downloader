60f1807680cf8db7eab92b0b3a103570cfb39cb0
Przemek <pk.korzen1@gmail.com>
2024-02-21 17:47:34 +0100

    changed method signature to return DTO instead of branch model and wrote serivce implementation

--- src/main/java/com/example/demo/services/GitHubService.java
+++ src/main/java/com/example/demo/services/GitHubService.java
@@ -2,11 +2,10 @@ package com.example.demo.services;
 
 import com.example.demo.dto.GitHubBranchDTO;
 import com.example.demo.dto.GitHubRepoDTO;
-import com.example.demo.model.repo.GithubRepo;
 
 import java.util.List;
 
 public interface GitHubService {
     List<GitHubRepoDTO> findReposByUsername(String username);
-    List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, GithubRepo repo);
+    List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, String repoName);
 }
--- src/main/java/com/example/demo/services/implementations/GitHubServiceImpl.java
+++ src/main/java/com/example/demo/services/implementations/GitHubServiceImpl.java
@@ -1,22 +1,66 @@
 package com.example.demo.services.implementations;
 
+import com.example.demo.converters.GitHubBranchDtoConverter;
+import com.example.demo.converters.GitHubRepoDtoConverter;
 import com.example.demo.dto.GitHubBranchDTO;
 import com.example.demo.dto.GitHubRepoDTO;
+import com.example.demo.model.branch.Branch;
 import com.example.demo.model.repo.GithubRepo;
 import com.example.demo.services.GitHubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class GitHubServiceImpl implements GitHubService {
+    @Value("${github.api.address}")
+    private String gitHubApiAddress;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    private GitHubRepoDtoConverter gitHubRepoDtoConverter;
+
+    @Autowired
+    private GitHubBranchDtoConverter gitHubBranchDtoConverter;
+
+    public GitHubServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
     @Override
     public List<GitHubRepoDTO> findReposByUsername(String username) {
-        return Collections.emptyList();
+        ResponseEntity<GithubRepo[]> responseEntity = restTemplate.getForEntity(gitHubApiAddress + "/users/" +
+                username + "/repos", GithubRepo[].class);
+        GithubRepo[] githubReposArray = responseEntity.getBody();
+        List<GitHubRepoDTO> gitHubRepoDTOS = new ArrayList<>();
+        if (Objects.nonNull(githubReposArray)) {
+            gitHubRepoDTOS = Arrays
+                    .stream(githubReposArray)
+                    .map(repo -> gitHubRepoDtoConverter.apply(repo,
+                            findBranchesByUsernameAndRepo(repo.getOwner().getLogin(), repo.getName())))
+                    .toList();
+        }
+        return gitHubRepoDTOS;
     }
+
     @Override
-    public List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, GithubRepo repo) {
-        return List.of();
+    public List<GitHubBranchDTO> findBranchesByUsernameAndRepo(String username, String repoName) {
+        ResponseEntity<Branch[]> responseEntity = restTemplate.getForEntity(gitHubApiAddress + "/repos/" +
+                username + "/" + repoName + "/branches", Branch[].class);
+        Branch[] branchesArray = responseEntity.getBody();
+        List<GitHubBranchDTO> gitHubBranchDTOS = new ArrayList<>();
+        if (Objects.nonNull(branchesArray)) {
+            gitHubBranchDTOS = Arrays
+                    .stream(branchesArray)
+                    .map(branch -> gitHubBranchDtoConverter.apply(branch))
+                    .toList();
+        }
+        return gitHubBranchDTOS;
     }
 }
