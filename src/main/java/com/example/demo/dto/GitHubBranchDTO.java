9388b0cd1076dbbe95aa31a32223c305ccdc7391
Przemek <pk.korzen1@gmail.com>
2024-02-21 15:39:42 +0100

    created DTOs

---
+++ src/main/java/com/example/demo/dto/GitHubBranchDTO.java
@@ -0,0 +1,6 @@
+package com.example.demo.dto;
+
+public class GitHubBranchDTO {
+    String name;
+    String commitSha;
+}
---
+++ src/main/java/com/example/demo/dto/GitHubRepoDTO.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class GitHubRepoDTO {
+    private String name;
+    private String ownerLogin;
+    private List<GitHubBranchDTO> branches;
+}
